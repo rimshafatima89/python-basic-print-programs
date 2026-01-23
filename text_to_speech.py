1b3e7c6f155183662b47b4b0a8d4f576835f8b38
RIMSHA FATIMA <fatimarimsha456@gmail.com>
2026-01-23 00:10:23 -0800

    Update text_to_speech.py

--- text_to_speech.py
+++ text_to_speech.py
@@ -5,4 +5,5 @@ engine = pyttsx3.init()
 # say() = telling what to speak
 engine.say("My Name is Rimsha Fatima and i am recently graduate in Artifical Inteligence")
 # runAndWait() = actually speaking
+
 engine.runAndWait()
